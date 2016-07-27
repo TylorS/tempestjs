c9abffbcb9f0c70e9a5077e503e6e8ecdd9c21ef
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-07-26 23:39:26 -0400

    chore(take): fix rollup config to use proper package name
    affects: @tempest/take

--- packages/take/rollup.config.js
+++ packages/take/rollup.config.js
@@ -2,9 +2,9 @@ import buble from 'rollup-plugin-buble'
 
 export default {
   entry: 'lib/index.js',
-  dest: 'dist/tempest-map.js',
+  dest: 'dist/tempest-take.js',
   format: 'umd',
-  moduleName: 'tempestMap',
+  moduleName: 'tempestTake',
   sourceMap: true,
   globals: {
     '@tempest/core': 'tempestCore'
