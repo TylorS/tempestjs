7e7fab4582558403f73e66c475f624d26dce61b1
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-03 16:41:24 -0400

    test(map): test currying of external API
    affects: @tempest/map

--- packages/map/test/index.ts
+++ packages/map/test/index.ts
@@ -11,4 +11,13 @@ describe ('map', () => {
       assert(x === 2)
     }, done, () => done())
   })
+
+  it ('should be curried', (done) => {
+    const f = (x: number) => x + 1
+    const stream = Stream.of(1)
+    const add1 = map(f)
+    add1(stream).subscribe((x: number) => {
+      assert(x === 2)
+    }, done, () => done())
+  })
 })
